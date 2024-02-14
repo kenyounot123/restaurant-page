b98a2bca3df7ec9936b0ba6f0cf71062b0d6aa60
kenyounot123 <kenlu519@gmail.com>
2024-02-13 23:03:54 -0500

    Update grid menu cards

--- src/menu.js
+++ src/menu.js
@@ -6,6 +6,10 @@ import applePie from "/src/apple_pie.png"
 
 const myImages = [fruit, applePie]
 const menuItems = ['Fruit Tarts', 'Apple Pies']
+const menuDescriptions = [
+  "A delectable pastry dessert with a flaky crust, creamy filling, and vibrant fresh fruits, offering a delightful blend of sweet and tangy flavors in every bite. Fruit tarts vary in size starting from $2.50 to $20",
+  "A classic dessert featuring a buttery, flaky crust filled with tender slices of apples, gently spiced and baked to golden perfection, evoking warm and comforting memories with every indulgent bite. Apple pies vary in size and go from $10 to $20"
+]
 
 function menuSection() {
 
@@ -26,8 +30,10 @@ function menuSection() {
     menuGrid.classList.add('menu-grid')
     //Create grid card for each menu item
     menuItems.forEach((item, index) => {
+      //Grid Card
       let menuCard = document.createElement('div')
       menuCard.classList.add('menu-card')
+      //Grid title and image
       let menuItem = document.createElement('h3')
       let menuItemImg = document.createElement('img')
       menuItemImg.classList.add('menu-card-img')
@@ -35,14 +41,20 @@ function menuSection() {
       menuItem.textContent = `${item}`
       menuItemImg.setAttribute('src', myImages[index])
       menuItemImg.setAttribute('alt', `${item}`)
+      //Grid text content 
+      let menuContent = document.createElement('p')
+      menuContent.classList.add('menu-content')
+      menuContent.textContent = menuDescriptions[index]
+
 
 
 
 
       
-      menuCard.append(menuItem)
-      menuCard.append(menuItemImg)
-      menuGrid.append(menuCard)
+      menuCard.appendChild(menuItem)
+      menuCard.appendChild(menuItemImg)
+      menuCard.appendChild(menuContent)
+      menuGrid.appendChild(menuCard)
     })
     menuContainer.appendChild(menuGrid)
     content.appendChild(menuContainer)
